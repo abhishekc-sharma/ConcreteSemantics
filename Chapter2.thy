1d638bb95a49ef0afd03a320fb70db7c8607ddf2
Abhishek Chandramouli Sharma <abhishekcs@protonmail.com>
2021-11-11 00:26:49 -0800

    Exercises 2.6-2.9

--- Chapter2.thy
+++ Chapter2.thy
@@ -88,4 +88,63 @@ theorem sum_upto_n [simp]: "sum_upto n = n * (n + 1) div 2"
   apply(auto)
 done
 
+(* Exercise 2.6 *)
+
+datatype 'a tree = Tip | Node "'a tree" 'a "'a tree"
+
+fun contents :: "'a tree \<Rightarrow> 'a list" where
+"contents Tip = []" |
+"contents (Node l a r) = (contents l)@[a]@(contents r)"
+
+fun sum_tree :: "nat tree \<Rightarrow> nat" where
+"sum_tree Tip = 0" |
+"sum_tree (Node l a r) = (sum_tree l) + a + (sum_tree r)"
+
+theorem sum_tree_eq_sum_list_contents : "sum_tree t = sum_list (contents t)"
+  apply(induction t)
+  apply(auto)
+done
+
+(* Exercise 2.7 *)
+
+fun mirror :: "'a tree \<Rightarrow> 'a tree" where
+"mirror Tip = Tip" |
+"mirror (Node l a r) = Node (mirror r) a (mirror l)"
+
+fun pre_order :: "'a tree \<Rightarrow> 'a list" where
+"pre_order Tip = []" |
+"pre_order (Node l a r) = a#(pre_order l)@(pre_order r)"
+
+fun post_order :: "'a tree \<Rightarrow> 'a list" where
+"post_order Tip = []" |
+"post_order (Node l a r) = (post_order l)@(post_order r)@[a]"
+
+theorem pre_order_mirror_eq_rev_post_order : "pre_order (mirror t) = rev (post_order t)"
+  apply(induction t)
+  apply(auto)
+done
+
+(* Exercise 2.8 *)
+
+fun intersperse :: "'a \<Rightarrow> 'a list \<Rightarrow> 'a list" where
+"intersperse _ [] = []" |
+"intersperse _ [x] = [x]" |
+"intersperse a (x#xs) = a#x#xs"
+
+theorem map_f_intersperse_eq_intersperse_map_f : "map f (intersperse a xs) = intersperse (f a) (map f xs)"
+  apply(induction xs rule: intersperse.induct)
+  apply(auto)
+done
+
+(* Exercise 2.9 *)
+
+fun itadd :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
+"itadd 0 n = n" |
+"itadd (Suc m) n = itadd m (Suc n)"
+
+theorem itadd_eq_add : "itadd m n = m + n"
+  apply(induction m arbitrary: n)
+  apply(auto)
+done
+
 end
